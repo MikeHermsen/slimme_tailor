8b0a9b452a8c4675730e6c39878dbd700a9ad863
MikeHermsen <mikehermsenprive@gmail.com>
2022-06-27 09:34:28 +0200

    first commit

---
+++ resources/views/layouts/base/sections/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base.base')
+
+@section('content')
+
+    <div>
+        <h1 class="bg-red-200">Hello world</h1>
+        <p class="text-red-200">Hello world</p>
+    </div>
+
+@endsection
